00ac6ab8919cc8bfe7dd1bb158062da073797a68
subhroster2007 <subhroster2007@gmail.com>
2024-05-31 12:51:29 +0530

    update 3 files and delete 3 files

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -2,11 +2,6 @@ import "./App.css";
 import CurrencyConverter from "./components/CurrencyConverter";
 
 function App() {
-  // let myObj = {
-  //   name: "Subhro",
-  //   Bio: "Software Engineer",
-  // };
-
   return (
     <>
       <CurrencyConverter />
